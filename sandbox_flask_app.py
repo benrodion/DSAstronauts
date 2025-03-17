37e3db1ce63d115e3013109f6db3b1ddbe7f9f44
mona-borth <mborth@web.de>
2025-03-17 17:27:40 +0100

    implemented login logic without any saving of usernames/passwords yet, just making sure routes work. Also no signup logic yet. Had to fix some stuff in login.html

--- sandbox_flask_app.py
+++ sandbox_flask_app.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 
 app = Flask(__name__)  # create the instance of the flask class
 
-@app.route('/')
-def login_or_sign_up():
+app.config["SESSION_PERMANENT"] = False # not sure if this is the right configuration
+app.config["SESSION_TYPE"] = "filesystem" # not sure if this is the right configuration
+Session(app)
+
+@app.route('/', methods=["GET","POST"])
+def show_login_page():
+    if request.method == "POST": # this implements login logic, but not signup yet
+        if not request.form.get("loggroupid"):
+            return render_template('login_result.html', message = "Group name missing")
+        elif not request.form.get("logpass"):
+            return render_template('login_result.html', message = "Password missing")
+        session["groupid"] = request.form.get("loggroupid")
+        session["password"] = request.form.get("logpass")
+        return render_template('login_result.html', message = "You are logged in")
     return render_template('login.html')
 
 if __name__ == "__main__":
