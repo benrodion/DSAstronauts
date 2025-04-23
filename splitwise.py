e5dd55dde7c4e7d60f5aa24313a8f548930914d9
benrodion <ben@anderer.de>
2025-04-23 23:14:15 +0200

    fixed major bug in the algorithm: cases where the a balance is not settled after it has been iterated upon are now handled correctly

--- splitwise.py
+++ splitwise.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from math import inf
+from math import inf, isclose
 
 
 class OptimalSplit:
@@ -36,7 +36,7 @@ class OptimalSplit:
 
         def dfs(start, balances, people):
             # skip over people with balance == 0 (settled debts)
-            while start < len(balances) and balances[start] == 0:
+            while start < len(balances) and abs(balances[start]) < 1e-9: # floating point safeguard 
                 start += 1
             # base case: there are no balances to settle --> return 0 (0 payments need to be made) and empty list of transactions    
             if start == len(balances):
@@ -48,28 +48,48 @@ class OptimalSplit:
             # attempt debt settling by pairing balances[start] with each of the later people in the list 
             for i in range(start + 1, len(balances)):
                 if balances[start] * balances[i] < 0:# ensures that only debtors & creditors can be paired, not debtor & debtor or creditor & creditor
-                    # Try settling start with i
-                    balances[i] += balances[start] # simulate payment --> adjust i's balance as if they'd received/given money
-                    amount = min(abs(balances[start]), abs(balances[i] - balances[start]))#amount being paid in the simulated transaction 
-                    
-                    # determine direction of payment: from start to i or from i to start
-                    if balances[start] < 0:
+                    amount = min(abs(balances[start]), abs(balances[i]))#amount to be paid in the simulated transaction 
+                    #save balance-values for backtracing
+                    start_original = balances[start]
+                    i_original = balances[i]
+
+                    # This block is mainly helpful for debugging with print-statements.
+                    # It determine direction of payment: from start to i or from i to start. 
+                    # It also simulates the payments for us to keep track of them. 
+                    if balances[start] < 0: #Case 1: if `start` owes money
                         path = [(people[start], people[i], amount)]
-                    else:
+                        #reflect the changes of the transactions in the balances list 
+                        balances[i] -= amount
+                        balances[start] += amount
+                    else:  #Case 2: if `start` gets money
                         path = [(people[i], people[start], amount)]
+                        balances[start] -= amount
+                        balances[i] += amount
+
+                    # determine whether to move to the next person or stay with 'start' depending on whether `start` has been settled
+                    if isclose(balances[start], 0, abs_tol= 1e-6): # Case 1: `start` is settled and we move on 
+                        next_start = start + 1
+                    else:
+                        next_start = start # Case 2: `start` is not settled and we stay with it 
 
                     # recursive settlement of remaining debt 
-                    payments, next_path = dfs(start + 1, balances[:], people)
+                    payments, next_path = dfs(next_start, balances[:], people)
                     if payments + 1 < min_payments: # if path results in fewer payments than before, update best solution 
                         min_payments = payments + 1
                         best_path = path + next_path
 
-                    balances[i] -= balances[start]  # backtrack
+                    # restore original balances for next depth-first search
+                    balances[start] = start_original
+                    balances[i] = i_original
+                
+            return min_payments, best_path
+
 
         # run recursive function starting from first unsettled person     
         _, result = dfs(0, balances, people)
         
         return result
+    
 
 
-   
+
